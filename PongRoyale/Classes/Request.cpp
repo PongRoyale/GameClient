a066fb57f0ef7a078028f6b6b5876342b77e6dfc
monksc <ccmonks32@gmail.com>
2020-11-18 08:54:00 -0500

    Does a little more to networking. I am waiting on the backend to finish it. Networking code still doesnt compile though

--- PongRoyale/Classes/Request.cpp
+++ PongRoyale/Classes/Request.cpp
@@ -6,13 +6,19 @@
 //
 
 // MARK: TODO
-void updateGame(unsigned ballsScoredOn, unsigned ballsPassed)
+void updateGame(unsigned ballsScoredOn, unsigned ballsPassed, const ccHttpRequestCallback &callback)
 {
 
     HttpRequest* request = new (std :: nothrow) HttpRequest();
     request->setUrl(URL UPDATE); // In c++ you can concat strings by placing them together if they are constant strings
+
     request->setRequestType(HttpRequest::Type::GET);
-    //request->setResponseCallback(CC_CALLBACK_2 (HttpClientTest::onHttpRequestCompleted, this));
+
+    // TODO: Needs to beable to pass in parameters
+    request->setRequestData()
+
+
+    request->setResponseCallback(callback);
     
     HttpClient::getInstance()->sendImmediate(request);
     
@@ -20,13 +26,23 @@ void updateGame(unsigned ballsScoredOn, unsigned ballsPassed)
 }
 
 // MARK: TODO
-void login(char * username, char * password) 
+// login('username', 'password', CC_CALLBACK_2 (HttpClientTest::onHttpRequestCompleted, this));
+void login(char * username, char * password, const ccHttpRequestCallback &callback) 
 {
 
     HttpRequest* request = new (std :: nothrow) HttpRequest();
     request->setUrl(URL LOGIN); // In c++ you can concat strings by placing them together if they are constant strings
     request->setRequestType(HttpRequest::Type::GET);
-    //request->setResponseCallback(CC_CALLBACK_2 (HttpClientTest::onHttpRequestCompleted, this));
+    request->setResponseCallback(callback);
+
+
+    const char * j1 = "{'username': '"
+    const char * j2 = "', 'password': '"
+    const char * j3 = "'}"
+
+    // TODO their is a better way to make request
+    // Maybe not json
+    request->setRequestData();
     
     HttpClient::getInstance()->sendImmediate(request);
     
--- PongRoyale/Classes/Request.h
+++ PongRoyale/Classes/Request.h
@@ -24,9 +24,9 @@
 // MARK: TODO: need to add in callback to param for functions
 //
 
-void updateGame(unsigned ballsScoredOn, unsigned ballsPassed);
+void updateGame(unsigned ballsScoredOn, unsigned ballsPassed, const ccHttpRequestCallback &callback);
 
-void login(char * username, char * password);
+void login(char * username, char * password, const ccHttpRequestCallback &callback);
 
 //
 // MARK: TODO
@@ -34,5 +34,49 @@ void login(char * username, char * password);
 // Place other function that make request to server in here.
 // Put their URL/function_name above in the define part
 //
+
+//
+// Note:
+// The callback Functions look likes this
+//
+
+/**
+
+// TODO: I would look up how to get request first 
+// I dont know if this is the best way
+void HelloWorld::onHttpRequestCompleted( cocos2d::network::HttpClient *sender, cocos2d::network::HttpResponse *response )
+{
+    // The data will be placed in the buffer.
+    std::vector<char> * buffer = response->getResponseData( );
+    char *concatenated = ( char * ) malloc( buffer->size( ) + 1 );
+    std::string s2( buffer->begin( ), buffer->end( ) );
+    strcpy( concatenated, s2.c_str( ) );
+
+    // JSON Parser
+    Json *json = Json_create( concatenated );
+    int test1 = Json_getInt( json, "a", -1 );
+    const char *test2 = Json_getString( json, "b", "default" );
+    float test3 = Json_getFloat( json, "c", -1.0f );
+
+    // View the console
+    log( "HTTP Response : key a : %i", test1 );
+    log( "HTTP Response : key b : %s", test2 );
+    log( "HTTP Response : key c : %f", test3 );
+
+    // Delete the JSON object
+    Json_dispose( json );
+
+
+    if ( response->getResponseCode( ) == 200 )
+    {
+        printf( "Succeeded" );
+        return;
+    }
+    else
+    {
+        printf("Failed");
+    }
+}
+*/
 
 #endif // __REQUEST_H__
